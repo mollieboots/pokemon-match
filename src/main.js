7deebb982a38c9327cb001ce3da5dcc47e8e2d6d
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-07-19 11:36:54 -0700

    add mollie branch and pokeapi-js-wrapper

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
+const Pokedex = require('pokeapi-js-wrapper');
 
 $(document).ready(function() {
 
+});
+
+var P = new Pokedex.Pokedex({protocol: 'https'});
+P.getPokemonByName('eevee') // with Promise
+.then(function(response) {
+  console.log(response);
+})
+.catch(function(err) {
+  console.log(err)
 });
