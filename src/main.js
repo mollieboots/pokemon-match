ed1f86c5b8d15ecae9bb1816ece73568c713fd89
Cathy Bradley (ctb116) <catherinetybradley@gmail.com>
2020-07-21 11:55:47 -0700

    add API 'roadmap' comments

--- src/main.js
+++ src/main.js
@@ -3,16 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
 import { Pokedex } from "./pokemon-match.js";
-// const Pokedex = require('pokeapi-js-wrapper');
-let P = new Pokedex.Pokedex({ protocol: 'https' });
+
+//Separation of Logic - see lesson by the same name
+//import PokemonService class
+//create new instance of PokemonService - like how you create a new instance of an object using a constructor.
+//You can then use your PokemonService anywhere in your application
+
+let P = new Pokedex.Pokedex({ protocol: "https" });
+//this is a promise that is requesting information from your pokemon API
+//It looks like you got the same promise in pokemon-match.js file
+//Gonna leave comments there because this main.js should just be your front-end logic
+//front-end logic generally anything dealing with gathering information from the user and displaying information back to them
 
 $(document).ready(function () {
-    P.getPokemonHabitatsList() // with Promise
+  //When separating this front-end and back-end logic be aware the lesson Separation of Logic is assuming an async function
+  P.getPokemonHabitatsList() // with Promise
     .then(function (response) {
-        console.log(response)
-        $('.list').text(response.results[0].name)
+      console.log(response);
+      $(".list").text(response.results[0].name);
     })
     .catch(function (err) {
-        console.log(err)
+      console.log(err);
     });
 });
+
+//.then returns a Promise object
+// a promise can have three states: Pending, Fulfilled, Rejected
--- src/pokemon-match.js
+++ src/pokemon-match.js
@@ -1,34 +1,70 @@
-export const Pokedex = require('pokeapi-js-wrapper');
+//To call an API you have many choices. The two presented in LHTP are:
+//Use the fetch() method (see lessons Using Fetch)
+// OR
+//manually create promises and XMLHttpRequests objects (see lesson Promises in ES6)
+//the fetch() method does the above for you
+//which one to use can be a matter of preference or the needs of the API you are using.
 
-let P = new Pokedex.Pokedex({ protocol: 'https' });
+//Using fetch() is further refactored in the lesson Async and Await
+//You could use fetch() without async functions
+//If you find yourself chaining multiple .then() (a prototype of fetch()) and your code is getting long - async functions are a solution
 
-P.getPokemonByName('gastly') // with Promise
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
+import { Pokedex } from "./pokemon-match.js";
+export const Pokedex = require("pokeapi-js-wrapper");
 
-// return an array of info, example below returns clefable, persim berry, and static
-P.resource(['/api/v2/pokemon/36', 'api/v2/berry/8', 'https://pokeapi.co/api/v2/ability/9/'])
-    .then(function(response) {
-      console.log(response); // resource function accepts singles or arrays of URLs/paths
-    })
+//This is your promise - your ticket at the DMV
+let P = new Pokedex.Pokedex({ protocol: "https" });
 
-P.getPokemonColorByName('green') // with Promise
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
+//This has the setup of using fetch without actually using the method itself
+//This is okay, without knowing anything about the pokemon API - perhaps getPokemonByName method is doing this for you
+P.getPokemonByName("gastly")
+  .then(function (response) {
+    //I added this "if response is not okay" three lines of code
+    //We have to determine what constitutes an error
+    //otherwise .catch() won't trigger - it only triggers if code throws an exception
+    //see lesson Exception Handling in JavaScript
+    //however - see the lesson Using Fetch() - at the bottom where it starts with "An important note"
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    console.log(response);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//When you are ready - see lesson Async and Await to refactor this block by using the async keyword
+//async functions are using promises under the hood
+//be aware that catch block from "Using Fetch" is used differently as a try..catch block if using an Async function
+//revisit the last paragraph in this lesson and discuss the possible downsides
+P.resource([
+  "/api/v2/pokemon/36",
+  "api/v2/berry/8",
+  "https://pokeapi.co/api/v2/ability/9/",
+]).then(function (response) {
+  console.log(response); // resource function accepts singles or arrays of URLs/paths
+});
+
+P.getPokemonColorByName("green") // with Promise
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 // returns "Likes to thrash about"
 P.getCharacteristicById(8) // with Promise
-    .then(function (response) {
-        console.log(response.descriptions[1].description);
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
+  .then(function (response) {
+    console.log(response.descriptions[1].description);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//Separation of Logic
+
+//   export class PokemonService {
+//     all the API calls and error handling
+//     return jsonifiedResponse
+//   }
